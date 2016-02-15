6aebde2be6a38f37439d63a1b2a9075dd496542e
panzertime <panzer.time@gmail.com>
2016-02-14 19:52:36 -0700

    update before branch

--- dl_loop.pl
+++ dl_loop.pl
@@ -6,11 +6,10 @@
 $size = 0;
 
 $url = $ARGV[0];
-$file = $ARGV[1];
+$file = chomp $ARGV[1];
 
 @args = ("curl", $url, "-o", $file);
-@del_args = ("rm", $file);
-@mv_args = ("mv", $file.".stub");
+@mv_args = ("mv", $file, $file.".stub");
 
 while("true"){
 	system(@args);
@@ -22,8 +21,11 @@ while("true"){
 			system(@mv_args) == 0
 				or die "could not move stub, major error";
 		}
-		system(@del_args) == 0
-			or die "could not delete stub, major error";
+		else {
+			@del_args = ("mv", $file, $file.$size);
+			system(@del_args) == 0
+				or die "could not delete stub, major error";
+		}
 	}
 	else {
 		exit 0;
